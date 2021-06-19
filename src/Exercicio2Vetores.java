4e9c10f18cc2326eec64bb7a7631872fae474106
Karen Calleri <ke.calleri@gmail.com>
2021-06-19 10:55:48 -0300

    Correção dos exercicios do módulo

--- src/Exercicio2Vetores.java
+++ src/Exercicio2Vetores.java
@@ -1,12 +1,36 @@
+import java.util.Scanner;
 
 public class Exercicio2Vetores {
-
-	public static void main(String[] args) {
-		// 2. O professor Wuni Jim Yensonal precisa de um programa para ajudar com um problema simples: para uma turma com 50 alunos precisa calcular a média da sala e descobrir quais são os alunos que precisarão de um atendimento personalizado. O fluxo do programa deve ser o seguinte:
-	//	A. Digitar as 50 notas em um vetor e, para cada nota digitada, digitar o nome do aluno correspondente em outro vetor
-	//	B. Calcular a média das notas digitadas
-	//	C. Para cada uma das notas que foi digitada e ficou abaixo da média, exibir uma mensagem que diga “Chamar o aluno para conversar”
-
-	}
-
+	
+	/*O professor Wuni Jim Yensonal precisa de um programa para ajudar com um problema simples: para uma turma com 50 alunos precisa calcular a média da sala e descobrir quais são os alunos que precisarão de um atendimento personalizado. O fluxo do programa deve ser o seguinte: 
+	*/
+	    public static void main(String[] args) {
+	        Scanner leitor = new Scanner(System.in);
+	        double notas[] = new double[3];
+	        String nomes[] = new String[3];
+	        double media=0;
+	        //A. Digitar as 50 notas em um vetor e, para cada nota digitada, digitar o nome do aluno correspondente em outro vetor
+	        for(int i=0;i<notas.length;i++) {
+	            System.out.println("Por favor digite o nome do aluno " + (i+1));
+	            nomes[i] = leitor.nextLine(); //Pode usar o next!!!
+	            System.out.println("Por favor digite a nota do aluno " + (i+1));
+	            notas[i] = leitor.nextDouble();
+	            leitor.nextLine();//Como estamos usando o nextLine na linha 16, precisamos do nosso POG para tirar o "ENTER" do buffer
+	        }
+	        //B. Calcular a média das notas digitadas
+	        //Para calcular a média, sugiro fazer um loop para ir somando as notas e fora do loop dividir pela quantidade de notas ;)
+	        for(int i=0;i<notas.length;i++) {
+	            media = media + notas[i];
+	        }
+	        media = media / notas.length;
+	        System.out.println("\n\nA média da turma é " + media);
+	        //C. Para cada uma das notas que foi digitada e ficou abaixo da média, exibir uma mensagem que diga “Chamar o aluno para conversar
+	        //Para essa etapa, sugiro percorrer o vetor, olhando cada uma das posições e comparando com a média. Se for menor, exibir a mensagem.
+	        for(int i=0;i<notas.length;i++) {
+	            if (notas[i] < media) {
+	                System.out.println("O(a) aluno(a) " + nomes[i] + " tirou nota " + notas[i] + ", e está abaixo da média. Chamar para conversar");
+	            }
+	        }
+	        leitor.close();
+	    }
 }
--- src/Exercicio3Vetores.java
+++ src/Exercicio3Vetores.java
@@ -1,15 +1,27 @@
+import java.util.Random;
 
 public class Exercicio3Vetores {
+	
+	// 3. Faça um algoritmo que gere aleatoriamente através de um laço de repetição PARA um vetor de 50 elementos inteiros (entre 0 - 9), calcule e exiba:
+		//	• A quantidade de números entre 3 e 7;
+		//	• Quantidade de números pares;
+		//	• A quantidade de números menores que 3;
+		//	• Quantidade de números ímpares.
+		//	Obs.: O vetor com os 50 elementos deve ser impresso na tela também.//
 
 	public static void main(String[] args) {
-		// 3. Faça um algoritmo que gere aleatoriamente através de um laço de repetição PARA um vetor de 50 elementos inteiros (entre 0 - 9), calcule e exiba:
-	//	• A quantidade de números entre 3 e 7;
-	//	• Quantidade de números pares;
-	//	• A quantidade de números menores que 3;
-	//	• Quantidade de números ímpares.
-	//	Obs.: O vetor com os 50 elementos deve ser impresso na tela também.//
-
 		
+		int aleatorios[] = new int[50];
+		Random gerador = new Random();
+		//PREENCHENDO COM ALEATÓRIOS
+		for(int i=0; i<aleatorios.length;i++) {
+		//gerador.nextInt(); gera um número inteiro entre 0 e um anterior ao que está entre parênteses
+		aleatorios[i] = gerador.nextInt(10);
+		}
+		for(int i=0; i<aleatorios.length;i++) {
+		System.out.println(aleatorios[i]);
+		}
+					
 		
 	}
 
